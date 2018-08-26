df88af680b53307891ffbb2d209e84b10a50e5f9
James Li <james.li@opower.com>
2018-08-25 20:30:47 -0700

    Updating serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -10,16 +10,16 @@ const urlsToCache = [
 	'./js/restaurant_info.js',
 	'./js/dbhelper.js',
 	'./data/restaurants.json',
-	'./img/1.jpg',
-	'./img/2.jpg',
-	'./img/3.jpg',
-	'./img/4.jpg',
-	'./img/5.jpg',
-	'./img/6.jpg',
-	'./img/7.jpg',
-	'./img/8.jpg',
-	'./img/9.jpg',
-	'./img/10.jpg'
+	'./imagemin-img/1-800.jpg',
+	'./imagemin-img/2-800.jpg',
+	'./imagemin-img/3-800.jpg',
+	'./imagemin-img/4-800.jpg',
+	'./imagemin-img/5-800.jpg',
+	'./imagemin-img/6-800.jpg',
+	'./imagemin-img/7-800.jpg',
+	'./imagemin-img/8-800.jpg',
+	'./imagemin-img/9-800.jpg',
+	'./imagemin-img/10-800.jpg'
 ];
 
 const cacheName = 'restaurant-cache-v1';
